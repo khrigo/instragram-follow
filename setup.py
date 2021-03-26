eaad68d2b7d80caaa64a6b7eb32ce1d6b09633fb
Igor Khripchenko <ikhripchenko@gmail.com>
2021-03-27 01:45:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'instagram-follow',
   packages = ['instagram_follow', 'instagram_follow.bot', 'instagram_follow.api'],
-  version = '0.1',
+  version = '0.1.1',
   description = 'Instagram scripts and API python wrapper.',
   author = 'Igor Khripchenko',
   author_email = 'ikhripchenko@gmail.com',
